e0a3e1e50fb69432a94566933caa4d21a339c01f
1tcmarki <126754383+1tcmarki@users.noreply.github.com>
2023-03-13 11:14:41 +0100

    of.txt hozzaadva , 5. feladat

--- tanfel.py
+++ tanfel.py
@@ -7,8 +7,8 @@ beosztas={}
 beosztasok=[]
 segedlista=[]
 
-with open("beosztas.txt","r",encoding="utf-8") as fm1:
-    for sor in fm1:
+with open("beosztas.txt","r",encoding="utf-8") as fin:
+    for sor in fin:
         segedlista.append(sor.strip())
         if len(segedlista)==4:
 
@@ -56,3 +56,19 @@ def tanar_oraszamanak_osszegzese(bok,be_nev):
             osszeg+=elem["oraszam"]
     return osszeg
 print(f"A tanar heti oraszama: {tanar_oraszamanak_osszegzese(beosztasok,be_tanarnev)}")
+
+"""
+5. feladat
+Készítse el az of.txt fájlt, amely az osztályfőnökök nevét tartalmazza osztályonként
+az alábbi formában (az osztályok megjelenítésének sorrendje a mintától eltérhet):
+
+9.a - Albatrosz Aladin
+9.b - Hangya Hanna
+9.c - Zerge Zenina
+KIVALOGATAS
+…
+"""
+with open("of.txt","w",encoding="utf-8") as fout:
+    for elem in beosztasok:
+        if elem["tantargy"]=="osztalyfonoki":
+            print(f"{elem['osztaly']} - {elem['tanar']}", file=fout)
